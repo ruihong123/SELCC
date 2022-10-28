8aa2bac3c6fc66444938e8f9b0a0c7821df3090f
ruihong123 <wang4996@purdue.edu>
2022-10-28 18:02:02 -0400

    debugging

--- btr/Btr.cpp
+++ btr/Btr.cpp
@@ -2595,10 +2595,15 @@ bool Btr::acquire_local_lock(Local_Meta *local_lock_meta, CoroContext *cxt, int 
 //    assert(local_lock_meta->issued_ticket - local_lock_meta->current_ticket <= 16 );
     //TOTHINK(potential bug): what if the ticket out of buffer.
     uint8_t expected = 0;
+#ifndef NDEBUG
+    uint64_t spin_coutner = 0;
+#endif
     while(!__atomic_compare_exchange_n(&local_lock_meta->local_lock_byte, &expected, 1, false, mem_cst_seq, mem_cst_seq)){
-
+        spin_coutner++;
     }
-//    global_static_var = __atomic_load_n((uint64_t*)local_lock_meta, (int)std::memory_order_seq_cst);
+    global_static_var = __atomic_load_n((uint64_t*)local_lock_meta, (int)std::memory_order_seq_cst);
+    printf("Acquire lock for %p, the current ticks is %d, issued ticket is%d, spin %lu times", local_lock_meta,
+           local_lock_meta->current_ticket, local_lock_meta->issued_ticket, spin_coutner);
 //    uint32_t ticket = lock_val << 32 >> 32;//clear the former 32 bit
 //    uint8_t current = __atomic_load_n(&local_lock_addr->current_ticket, mem_cst_seq);// current is the former 32 bit in ticket lock
 //    uint8_t current = local_lock_meta->current_ticket;
