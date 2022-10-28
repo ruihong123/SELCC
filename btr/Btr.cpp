03cc21a5009a1091252149e1f19ee72f865aa3c8
ruihong123 <wang4996@purdue.edu>
2022-10-27 23:30:32 -0400

    debugging

--- btr/Btr.cpp
+++ btr/Btr.cpp
@@ -1662,10 +1662,12 @@ bool Btr::internal_page_store(GlobalAddress page_addr, Key &k, GlobalAddress &v,
 #ifndef NDEBUG
         usleep(4);
         uint8_t expected = 0;
-        assert(!__atomic_compare_exchange_n(&page->local_lock_meta.local_lock_byte, &expected, 1, false, mem_cst_seq, mem_cst_seq));
-#endif
-
         assert( __atomic_load_n(&page->local_lock_meta.local_lock_byte, mem_cst_seq) != 0);
+        assert(!__atomic_compare_exchange_n(&page->local_lock_meta.local_lock_byte, &expected, 1, false, mem_cst_seq, mem_cst_seq));
+
+
+
+#endif
         if (handover){
             // No need to read the page again because we handover the page as well.
 //            rdma_mg->RDMA_Read(page_addr, local_buffer, kInternalPageSize, IBV_SEND_SIGNALED, 1, Internal_and_Leaf);
