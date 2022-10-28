5ddecfbc8600ab951d26b167f135c5565ded38a6
ruihong123 <wang4996@purdue.edu>
2022-10-28 17:45:37 -0400

    debugging

--- btr/Btr.cpp
+++ btr/Btr.cpp
@@ -2581,6 +2581,9 @@ inline bool Btr::acquire_local_lock(GlobalAddress lock_addr, CoroContext *cxt,
 
   return node.hand_over;
 }
+    uint64_t global_static_var;
+//    = __atomic_load_n((uint64_t*)&page->local_lock_meta, (int)std::memory_order_seq_cst);
+
 bool Btr::acquire_local_lock(Local_Meta *local_lock_meta, CoroContext *cxt, int coro_id) {
     assert((uint64_t)local_lock_meta % 8 == 0);
     //TODO: local lock implementation over InternalPage::Local_Meta.
@@ -2595,6 +2598,7 @@ bool Btr::acquire_local_lock(Local_Meta *local_lock_meta, CoroContext *cxt, int 
     while(!__atomic_compare_exchange_n(&local_lock_meta->local_lock_byte, &expected, 1, false, mem_cst_seq, mem_cst_seq)){
 
     }
+    global_static_var = __atomic_load_n((uint64_t*)local_lock_meta, (int)std::memory_order_seq_cst);
 //    uint32_t ticket = lock_val << 32 >> 32;//clear the former 32 bit
 //    uint8_t current = __atomic_load_n(&local_lock_addr->current_ticket, mem_cst_seq);// current is the former 32 bit in ticket lock
 //    uint8_t current = local_lock_meta->current_ticket;
