ff0cffb38785a398d718d1f6d0ea79de9167a2aa
ruihong123 <wang4996@purdue.edu>
2022-10-29 17:19:42 -0400

    debugging

--- include/Common.h
+++ include/Common.h
@@ -110,7 +110,7 @@ constexpr int64_t kPerCoroRdmaBuf = 128 * 1024;
 
 constexpr uint8_t kMaxHandOverTime = 8;
 
-constexpr int kIndexCacheSize = 10000; // MB
+constexpr int kIndexCacheSize = 6400000; // MB
 } // namespace define
 
 static inline unsigned long long asm_rdtsc(void) {
