53f1cee3ef5d40d29c34ac93d4c5eacb88a3d9e9
ruihong123 <wang4996@purdue.edu>
2022-11-11 17:01:14 -0500

    make the door bell batch only signal the last work request.

--- port/port_posix.cc
+++ port/port_posix.cc
@@ -153,6 +153,8 @@ void RWMutex::ReadLock() {
 
     PthreadCall("read lock", pthread_rwlock_rdlock(&mu_));
 #ifndef NDEBUG
+    assert(lock_state_ == 0);
+
     lock_state_ = 1;
 #endif
 }
@@ -160,17 +162,24 @@ void RWMutex::ReadLock() {
 void RWMutex::WriteLock() {
     PthreadCall("write lock", pthread_rwlock_wrlock(&mu_));
 #ifndef NDEBUG
+    assert(lock_state_ == 0);
     lock_state_ = 2;
 #endif
 }
 
 void RWMutex::ReadUnlock() {
+#ifndef NDEBUG
     assert(lock_state_ == 1);
+    lock_state_ = 0;
+#endif
     PthreadCall("read unlock", pthread_rwlock_unlock(&mu_));
 }
 
 void RWMutex::WriteUnlock() {
+#ifndef NDEBUG
     assert(lock_state_ == 2);
+    lock_state_ = 0;
+#endif
     PthreadCall("write unlock", pthread_rwlock_unlock(&mu_));
 }
 
