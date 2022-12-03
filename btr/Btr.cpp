977cec681883f90bf469ae18e14c2c209c0af88d
ruihong123 <wang4996@purdue.edu>
2022-12-02 23:25:51 -0500

    RDMA connection accross the compute nodes.

--- btr/Btr.cpp
+++ btr/Btr.cpp
@@ -80,8 +80,7 @@ static void Deallocate_MR_WITH_CCP(const GlobalAddress g_ptr, void* value, int s
             // RDMA read unlock
             printf("release the read lock during the handle destroy\n ");
             Btr::rdma_mg->global_RUnlock(lock_gptr, Btr::rdma_mg->Get_local_CAS_mr());
-            //TODO: delete the line below.
-            assert(false);
+
         }else if(remote_lock_status == 2){
 
             // TODO: shall we not consider the global lock word when flushing back the page?
