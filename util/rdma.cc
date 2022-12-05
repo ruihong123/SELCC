faf1d9c12d136c9f01aad9c4f7d0fbaf3cc988a1
ruihong123 <wang4996@purdue.edu>
2022-12-04 21:51:45 -0500

    RDMA connection accross the compute nodes.

--- util/rdma.cc
+++ util/rdma.cc
@@ -3247,7 +3247,7 @@ int RDMA_Manager::RDMA_CAS(ibv_mr *remote_mr, ibv_mr *local_mr, uint64_t compare
         // TODO: if the starvation bit is on then we release and wait the lock.
         if ( (return_value >> 56) > 0  ){
             assert(false);
-            assert((return_value & (1ull << node_id)) == 0);
+//            assert((return_value & (1ull << (RDMA_Manager::node_id/2 + 1)))== 0);
 //            Prepare_WR_FAA(sr[0], sge[0], lock_addr, cas_buffer, -add, 0, Internal_and_Leaf);
             RDMA_FAA(lock_addr, cas_buffer, -add, IBV_SEND_SIGNALED, 1, Internal_and_Leaf);
             goto retry;
@@ -3326,7 +3326,7 @@ int RDMA_Manager::RDMA_CAS(ibv_mr *remote_mr, ibv_mr *local_mr, uint64_t compare
         //Only the second RDMA issue a completion
         RDMA_FAA(lock_addr, cas_buffer, -add, IBV_SEND_SIGNALED,1, Internal_and_Leaf);
         uint64_t return_data = (*(uint64_t*) cas_buffer->addr);
-        assert((return_data & (1ull << (RDMA_Manager::node_id/2 + 1))) == 1);
+        assert((return_data & (1ull << (RDMA_Manager::node_id/2 + 1))) != 0);
 
 //        printf("Release lock for %lu", lock_addr.offset-8);
     }
