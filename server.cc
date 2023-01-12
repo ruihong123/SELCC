a6113a47636a266b30998dad4c0c1d9a2b4df7b7
wang4996 <wang4996@purdue.edu>
2023-01-12 17:28:32 -0500

    upload updates

--- server.cc
+++ server.cc
@@ -7,6 +7,7 @@
 int main(int argc,char* argv[])
 {
   DSMEngine::Memory_Node_Keeper* mn_keeper;
+
   if (argc == 4){
     uint32_t tcp_port;
     int pr_size;
@@ -25,9 +26,11 @@ int main(int argc,char* argv[])
     //  strValue.str("");
     strValue3 << value;
     strValue3 >> Memory_server_id;
+      printf("TCP_PORT is %d, preregistered memory size is %d, node id is %d", tcp_port, pr_size, 2* Memory_server_id + 1);
      mn_keeper = new DSMEngine::Memory_Node_Keeper(true, tcp_port, pr_size);
      DSMEngine::RDMA_Manager::node_id = 2* Memory_server_id + 1;
   }else{
+      printf("Please input  ./memory_server [TCP_PORT] [preregistered memory size] [node id]\n ");
     mn_keeper = new DSMEngine::Memory_Node_Keeper(true, 19843, 88);
     DSMEngine::RDMA_Manager::node_id = 1;
   }
