b4ce9b507bc192dd10f1f082ede93c1fc5a3ecd8
ruihong123 <wang4996@purdue.edu>
2023-04-27 20:22:59 -0400

    testing

--- util/cache.cc
+++ util/cache.cc
@@ -15,8 +15,8 @@
 #include "util/mutexlock.h"
 #include "HugePageAlloc.h"
 // DO not enable the two at the same time otherwise there will be a bug.
-#define BUFFER_HANDOVER
-//#define EARLY_LOCK_RELEASE
+//#define BUFFER_HANDOVER
+#define EARLY_LOCK_RELEASE
 uint64_t cache_miss[MAX_APP_THREAD][8];
 uint64_t cache_hit[MAX_APP_THREAD][8];
 uint64_t invalid_counter[MAX_APP_THREAD][8];
