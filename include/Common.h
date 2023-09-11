6d9307c2c9cf10dbceac3bde384566e9281690ec
ruihong123 <wang4996@purdue.edu>
2023-09-11 11:36:28 -0400

    test two node performance

--- include/Common.h
+++ include/Common.h
@@ -112,7 +112,7 @@ constexpr int64_t kPerCoroRdmaBuf = 128 * 1024;
 
 constexpr uint8_t kMaxHandOverTime = 8;
 
-constexpr int kIndexCacheSize = 2048; // MB
+constexpr int kIndexCacheSize = 8192; // MB
 } // namespace define
 
 static inline unsigned long long asm_rdtsc(void) {
