88e2f1595a511bec6505839230c3ce83bf367a7b
ruihong123 <wang4996@purdue.edu>
2023-09-12 21:13:58 -0400

    reduce cache coherence overhead.

--- util/rdma.cc
+++ util/rdma.cc
@@ -3702,9 +3702,10 @@ int RDMA_Manager::RDMA_CAS(ibv_mr *remote_mr, ibv_mr *local_mr, uint64_t compare
             *(uint64_t *)local_CAS_mr->addr = 0;
             assert(page_addr.nodeID == remote_lock_addr.nodeID);
             Batch_Submit_WRs(sr, 0, page_addr.nodeID);
+            printf("Release write lock for %lu\n");
             //TODO: it could be spuriously failed because of the FAA.so we can not have async
         }else{
-            printf("This code path shall not be entered\n");
+//            printf("This code path shall not be entered\n");
 
 //#ifndef NDEBUG
             uint64_t retry_cnt = 0;
