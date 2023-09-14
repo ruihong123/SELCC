f02c8e4a51e5d5e3fa13113bfa9ea7fbaaf64151
ruihong123 <wang4996@purdue.edu>
2023-09-14 16:06:54 -0400

    debug the FAA write unlock

--- util/rdma.cc
+++ util/rdma.cc
@@ -3705,7 +3705,7 @@ int RDMA_Manager::RDMA_CAS(ibv_mr *remote_mr, ibv_mr *local_mr, uint64_t compare
             *(uint64_t *)local_CAS_mr->addr = 0;
             assert(page_addr.nodeID == remote_lock_addr.nodeID);
             Batch_Submit_WRs(sr, 0, page_addr.nodeID);
-            assert(((*(uint64_t*) local_CAS_mr->addr) >> 56) == (add >> 56));
+//            assert(((*(uint64_t*) local_CAS_mr->addr) >> 56) == (add >> 56));
 //            printf("Release write lock for %lu\n",page_addr);
             //TODO: it could be spuriously failed because of the FAA.so we can not have async
         }else{
