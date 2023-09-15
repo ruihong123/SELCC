ee2fef1d46b9275c0e58af31cc0f6fe1057989d6
ruihong123 <wang4996@purdue.edu>
2023-09-15 17:16:39 -0400

    debug the FAA write unlock

--- util/rdma.cc
+++ util/rdma.cc
@@ -3324,17 +3324,17 @@ int RDMA_Manager::RDMA_CAS(ibv_mr *remote_mr, ibv_mr *local_mr, uint64_t compare
 //        printf("global read lock at %p \n", page_addr);
         retry:
         retry_cnt++;
-        if (retry_cnt % 20 ==  2) {
+        if (retry_cnt % 4 ==  2) {
 //            assert(compare%2 == 0);
-            if(retry_cnt < 100){
+            if(retry_cnt < 20){
                 //do nothing
-            }else if (retry_cnt <500){
-                usleep(50);
+            }else if (retry_cnt <100){
+                usleep(100);
             }else if (retry_cnt <1000){
                 if (retry_cnt == 1001){
                     printf("Invalidate RPC in Read lock timeout 1000\n");
                 }
-                usleep(100);
+                usleep(500);
             }
             else if (retry_cnt <2000){
                 if (retry_cnt == 2001) {
@@ -3524,17 +3524,17 @@ int RDMA_Manager::RDMA_CAS(ibv_mr *remote_mr, ibv_mr *local_mr, uint64_t compare
         //TODO: send an RPC to the destination every 4 retries.
         // Check whether the invalidation is write type or read type. If it is a read type
         // we need to broadcast the message to multiple destination.
-        if (retry_cnt % 20 ==  2) {
+        if (retry_cnt % 4 ==  2) {
             // exponential back up to avoid remote receive buffer overflow.
-            if(retry_cnt < 100){
+            if(retry_cnt < 20){
                 //do nothing
-            }else if (retry_cnt <500){
-                usleep(50);
+            }else if (retry_cnt <100){
+                usleep(100);
             }else if (retry_cnt <1000){
                 if (retry_cnt == 1001) {
                     printf("Invalidate RPC in write unlock timeout 1000\n");
                 }
-                usleep(100);
+                usleep(500);
             }
             else if (retry_cnt <2000){
                 if (retry_cnt == 2001) {
