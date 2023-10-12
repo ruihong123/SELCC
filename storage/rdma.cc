1e388e4fef2644b5a165692289f2ae521dcc531f
ruihong123 <wang4996@purdue.edu>
2023-10-12 18:24:40 -0400

    Enable the microbenchmark for CCProtocol

--- storage/rdma.cc
+++ storage/rdma.cc
@@ -3469,6 +3469,7 @@ int RDMA_Manager::RDMA_CAS(ibv_mr *remote_mr, ibv_mr *local_mr, uint64_t compare
             }
             assert(false);
         }
+        printf("Lock update successful\n");
         return true;
     }
     //TODO: Implement a sync read unlock function.
