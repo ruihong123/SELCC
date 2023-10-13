171f888c790d1c61b6f6494556febf2b1f97cb13
ruihong123 <wang4996@purdue.edu>
2023-10-12 23:11:32 -0400

    Enable the microbenchmark for CCProtocol

--- storage/rdma.cc
+++ storage/rdma.cc
@@ -3797,7 +3797,7 @@ int RDMA_Manager::RDMA_CAS(ibv_mr *remote_mr, ibv_mr *local_mr, uint64_t compare
                 goto retry;
             }
         }
-        page->global_lock = swap;
+        ((LeafPage<uint64_t,uint64_t>*)(page_buffer->addr))->global_lock = swap;
 //        printf("Acquire Write Lock at %lu\n", page_addr);
 //        assert(page_addr == (((LeafPage<uint64_t,uint64_t>*)(page_buffer->addr))->hdr.this_page_g_ptr));
     }
