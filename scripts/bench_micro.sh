d89325dfde3e268720c5cadbd086ff3af5170e17
ruihong123 <wang4996@purdue.edu>
2023-10-17 13:29:46 -0400

    Enable the microbenchmark for CCProtocol

--- scripts/bench_micro.sh
+++ scripts/bench_micro.sh
@@ -107,7 +107,7 @@ run() {
 #    echo `cat $slaves`
     echo $compute_num
     echo $memory_num
-  	read -r -a memcached_node <<< $(head -n 1 $SRC_HOME/memcached.conf)
+  	read -r -a memcached_node <<< $(head -n 1 $SRC_HOME/memcached_ip.conf)
   	echo "restart memcached on ${memcached_node[0]}"
     ssh -o StrictHostKeyChecking=no ${memcached_node[0]} "sudo service memcached restart"
     for memory in "${memory_nodes[@]}"
