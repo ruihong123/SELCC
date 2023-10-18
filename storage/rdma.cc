d145f7cfe8f62bb5f49625ae7e06ebb7507374b3
ruihong123 <wang4996@purdue.edu>
2023-10-18 14:53:43 -0400

    Enable the microbenchmark for CCProtocol

--- storage/rdma.cc
+++ storage/rdma.cc
@@ -1070,13 +1070,13 @@ void RDMA_Manager::Cross_Computes_RPC_Threads_Creator(uint16_t target_node_id) {
         std::unique_lock<std::mutex> lck(invalidate_channel_mtx);
 //        std::thread p_t(&::DSMEngine::RDMA_Manager::compute_message_handling_thread, this, target_node_id, i, recv_mr[i]);
         Invalidation_bg_threads.emplace_back(&RDMA_Manager::invalidation_message_handling_worker, this, target_node_id, i, recv_mr[i]);
-//        Invalidation_bg_threads.back().detach();
+        Invalidation_bg_threads.back().detach();
     }
 //    std::unique_lock<std::mutex> lck(invalidate_channel_mtx);
-    sleep(2);
-    for (auto &iter:Invalidation_bg_threads) {
-        iter.detach();
-    }
+//    sleep(2);
+//    for (auto &iter:Invalidation_bg_threads) {
+//        iter.detach();
+//    }
 
 }
 
