f0b4fc55b5580582d378c8db4e02b3055f10bd3b
ruihong123 <wang4996@purdue.edu>
2023-10-19 00:55:48 -0400

    Enable the microbenchmark for CCProtocol

--- scripts/bench_micro.sh
+++ scripts/bench_micro.sh
@@ -475,10 +475,10 @@ run_node_test() {
 echo "**************************run node test****************************"
 result_file=$bin/results/node
 node_range="8"
-thread_range="1 16"
+thread_range="16"
 remote_range="0"
 shared_range="50 100"
-read_range="50 100"
+read_range="0 50"
 space_range="0"
 time_range="0"
 op_range="0"
