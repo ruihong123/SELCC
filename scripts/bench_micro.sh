2658b7b7f81dad80fbfe9c9c2e344cf02c15efff
ruihong123 <wang4996@purdue.edu>
2023-10-25 12:59:41 -0400

    CPU scalability test

--- scripts/bench_micro.sh
+++ scripts/bench_micro.sh
@@ -474,7 +474,7 @@ run_node_test() {
 # node test
 echo "**************************run node test****************************"
 result_file=$bin/results/node
-node_range="1 2 4 8"
+node_range="1"
 thread_range="16"
 remote_range="0"
 shared_range="100"
