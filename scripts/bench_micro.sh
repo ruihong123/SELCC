7dd0bb478a5e31307add5d2dfe5c0a7c0b4206e7
ruihong123 <wang4996@purdue.edu>
2023-10-26 16:28:56 -0400

    CPU scalability test

--- scripts/bench_micro.sh
+++ scripts/bench_micro.sh
@@ -482,7 +482,7 @@ read_range="50"
 space_range="0"
 time_range="0"
 workload_range="1"
-zipfian_alpha_range="0.5 1 1.5"
+zipfian_alpha_range="1 1.5"
 op_range="1" # use 1
 #cache_th=0.5
 for workload in $workload_range
