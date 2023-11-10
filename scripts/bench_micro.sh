da6c1fd520dbf84ff9ea6cb789992e8da64d2c31
wang4996 <wang4996@purdue.edu>
2023-11-10 09:02:47 +0800

    debug

--- scripts/bench_micro.sh
+++ scripts/bench_micro.sh
@@ -474,7 +474,7 @@ run_node_test() {
 # node test
 echo "**************************run node test****************************"
 result_file=$bin/results/node
-node_range="4"
+node_range="8"
 thread_range="16"
 remote_range="0"
 shared_range="100"
