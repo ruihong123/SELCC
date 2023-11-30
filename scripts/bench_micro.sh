6aad04af66fec956bfc82ea225de0a293a6db800
wang4996 <wang4996@purdue.edu>
2023-11-30 16:04:39 +0800

    add no cache implementation

--- scripts/bench_micro.sh
+++ scripts/bench_micro.sh
@@ -2,7 +2,7 @@
 bin=`dirname "$0"`
 bin=`cd "$bin"; pwd`
 SRC_HOME=$bin/..
-BIN_HOME=$bin/../debug
+BIN_HOME=$bin/../release
 conf_file_all=$bin/../connection_cloudlab.conf
 conf_file=$bin/../connection.conf
 
@@ -478,7 +478,7 @@ node_range="1"
 thread_range="1 2 4 8 16"
 remote_range="0"
 shared_range="100"
-read_range="0"
+read_range="0 50 95 100"
 space_range="0"
 time_range="0"
 workload_range="0"
