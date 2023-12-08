e87aee9839e12af221114c26a01dc25d32c90119
wang4996 <wang4996@purdue.edu>
2023-12-08 21:28:13 +0800

    check why no cache benefit from space locality

--- test/benchmark_micro.cpp
+++ test/benchmark_micro.cpp
@@ -942,7 +942,7 @@ int main(int argc, char* argv[]) {
                 space_locality, time_locality, op_type, memory_type, item_size, static_cast<double>(invalidation_num) / ITERATION_TOTAL, static_cast<double>(hit_valid_num) / ITERATION_TOTAL,t_thr,
                 a_thr, a_lat);
 #ifdef GETANALYSIS
-        printf("Preread average time elapse is %lu ns, Postread average time elapse is %lu ns\n", PrereadTotal/Prereadcounter, PostreadTotal/Postreadcounter);
+        printf("Preread average time elapse is %lu ns, Postread average time elapse is %lu ns, Memcopy average time elapse is %lu ns\n", PrereadTotal/Prereadcounter, PostreadTotal/Postreadcounter, MemcopyTotal/Memcopycounter);
 #endif
         result.close();
     }
