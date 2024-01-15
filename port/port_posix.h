32f306569cfea9aed0a41f6c04f03f601149b6dd
ruihong123 <wang4996@purdue.edu>
2024-01-14 23:31:50 -0500

    Multi hotspot

--- port/port_posix.h
+++ port/port_posix.h
@@ -115,7 +115,7 @@ extern bool Show_Me_The_Print;
 #define SEND_OUTSTANDING_SIZE 4 // Increase this if we have a lot of async ops
 #define ASYNC_UNLOCK
 #define INVALIDATION_STATISTICS
-#define EXCLUSIVE_HOTSPOT // exclusive hotspot per compute. workload == 1.
+//#define EXCLUSIVE_HOTSPOT // exclusive hotspot per compute. workload == 1.
 #define ACCESS_MODE 1 // 0 No cache no sharding, 1 cache with CC protocol, 2 sharding.
 #include "port/thread_annotations.h"
 
