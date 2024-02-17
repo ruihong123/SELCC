40bc9232ec5fa0392de31717cb9c2f8fae5bee5a
ruihong123 <wang4996@purdue.edu>
2024-02-16 23:31:34 -0500

    Solution for lock starvation.

--- scripts/sync_all.sh
+++ scripts/sync_all.sh
@@ -87,6 +87,8 @@ function run_bench() {
     ssh -o StrictHostKeyChecking=no $node "pkill -f micro_bench"
     ssh -o StrictHostKeyChecking=no $node "pkill -f memory_server_term"
     ssh -o StrictHostKeyChecking=no $node "pkill -f tpcc"
+    ssh -o StrictHostKeyChecking=no $node "pkill -f micro_bench"
+    ssh -o StrictHostKeyChecking=no $node "echo '/proj/purduedb-PG0/logs/core$node' | sudo tee /proc/sys/kernel/core_pattern"
   done
   for node in ${compute_shard[@]}
   do
@@ -97,6 +99,8 @@ function run_bench() {
     ssh -o StrictHostKeyChecking=no $node "pkill -f micro_bench"
     ssh -o StrictHostKeyChecking=no $node "pkill -f memory_server_term"
     ssh -o StrictHostKeyChecking=no $node "pkill -f tpcc"
+    ssh -o StrictHostKeyChecking=no $node "echo '/proj/purduedb-PG0/logs/core$node' | sudo tee /proc/sys/kernel/core_pattern"
+
 
   done
   read -r -a memcached_node <<< $(head -n 1 $SRC_HOME/memcached_ip.conf)
