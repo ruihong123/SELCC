77297a4edd7225740180bc3ef6cd3991c6b3a1c2
ruihong123 <wang4996@purdue.edu>
2024-02-23 17:49:20 -0500

    Solution for lock starvation.

--- scripts/bench_micro.sh
+++ scripts/bench_micro.sh
@@ -480,7 +480,7 @@ run_node_test() {
 # node test
 echo "**************************run node test****************************"
 result_file=$bin/results/node
-node_range="8"
+node_range="2"
 thread_range="16"
 remote_range="100"
 shared_range="100"
