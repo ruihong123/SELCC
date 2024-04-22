22483452fe2b931332a245c57952a046f9107be2
ruihong123 <wang4996@purdue.edu>
2024-04-21 23:29:07 -0400

    modify the implementation of root node cache bypassing.

--- port/port_posix.h
+++ port/port_posix.h
@@ -120,7 +120,7 @@ extern bool Show_Me_The_Print;
 #define ACCESS_MODE 1 // 0 No cache no sharding, 1 cache with CC protocol, 2 sharding.
 #define SYNC_XALL_OFFSET (16384*2)
 #define GLOBAL_HANDOVER
-//#define PARALLEL_INVALIDATION
+#define PARALLEL_INVALIDATION
 #include "port/thread_annotations.h"
 
 namespace DSMEngine {
