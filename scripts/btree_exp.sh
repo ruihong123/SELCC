c7da48f8bf57550c0931631447cce0b0ff80d595
ruihong123 <wang4996@purdue.edu>
2024-05-13 15:11:51 -0400

    TPC-C benchmark analysis and btree benchmark scripts.

--- scripts/btree_exp.sh
+++ scripts/btree_exp.sh
@@ -111,8 +111,8 @@ run_tpcc () {
 #
 vary_thread_number () {
   #read_ratios=(0 30 50 70 90 100)
-  thread_number=(1 8 16 32)
-  read_ratio=(0 50 95 100)
+  thread_number=(8)
+  read_ratio=(0)
   for thread_n in ${thread_number[@]}; do
     for read_r in ${read_ratio[@]}; do
     compute_ARGS="$read_r $thread_n 0"
