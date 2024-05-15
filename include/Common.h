ae6845b8fdb3e27fe58ec0aef04784b74e74b418
ruihong123 <wang4996@purdue.edu>
2024-05-15 15:04:30 -0400

    TPC-C benchmark analysis and btree benchmark scripts.

--- include/Common.h
+++ include/Common.h
@@ -113,7 +113,8 @@ constexpr int64_t kPerCoroRdmaBuf = 128 * 1024;
 
 constexpr uint8_t kMaxHandOverTime = 8;
 
-constexpr int kIndexCacheSize = 8*1024ull; // MB
+//constexpr int kIndexCacheSize = 8*1024ull; // MB
+constexpr int kIndexCacheSize = 16*1024ull; // MB
 } // namespace define
 
 static inline unsigned long long asm_rdtsc(void) {
