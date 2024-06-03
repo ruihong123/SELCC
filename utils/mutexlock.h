44fb67b758c26d521e0efe57ad521bd3162c862a
ruihong123 <wang4996@purdue.edu>
2024-06-03 14:24:12 -0400

    Initial set up for Timestampbased concurrency control. Retest the btree YCSB benchmark.

--- utils/mutexlock.h
+++ utils/mutexlock.h
@@ -76,6 +76,7 @@ class SpinMutex {
   std::atomic<bool> locked_;
 };
 // TODO: This read-write implementation may not 100% guarantee the atomicity, is it?
+//  If it is not, we can follow the RDMA shared exclusive latch to implement an local atomic read-write lock.
 // Implement a shared-exclusive spin lock with lock, try_lock, unlock, lock_shared, try_lock_shared, and unlock_shared.
 class RWSpinLock{
     std::atomic<int> readers_count{0};
