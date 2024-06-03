6b8fb5477040afe6ef81a431425b7403bd8e76be
ruihong123 <wang4996@purdue.edu>
2024-06-03 00:09:31 -0400

    Initial set up for Timestampbased concurrency control. Retest the btree YCSB benchmark.

--- include/Common.h
+++ include/Common.h
@@ -114,7 +114,7 @@ constexpr int64_t kPerCoroRdmaBuf = 128 * 1024;
 constexpr uint8_t kMaxHandOverTime = 8;
 
 //constexpr int kIndexCacheSize = 8*1024ull; // MB
-constexpr int kIndexCacheSize = 512ull; // MB
+constexpr int kIndexCacheSize = 1024ull; // MB
 } // namespace define
 
 static inline unsigned long long asm_rdtsc(void) {
