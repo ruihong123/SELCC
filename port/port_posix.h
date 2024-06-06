2d922ea572efe1e913bd51767db10f855d18342f
wang4996 <wang4996@purdue.edu>
2024-06-06 15:17:39 -0500

    check why no cache benefit from space locality

--- port/port_posix.h
+++ port/port_posix.h
@@ -118,7 +118,7 @@ extern bool Show_Me_The_Print;
 #define ASYNC_UNLOCK
 #define INVALIDATION_STATISTICS
 //#define EXCLUSIVE_HOTSPOT // exclusive hotspot per compute. workload == 1.
-#define ACCESS_MODE 0 // 0 No cache no sharding, 1 cache with CC protocol, 2 sharding.
+#define ACCESS_MODE 1 // 0 No cache no sharding, 1 cache with CC protocol, 2 sharding.
 #define SYNC_XALL_OFFSET (16384*2)
 #define GLOBAL_HANDOVER
 #define PARALLEL_INVALIDATION
