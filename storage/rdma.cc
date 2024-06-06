544f6bd2d5c0380044cca64523569905489b59de
wang4996 <wang4996@purdue.edu>
2024-06-06 12:05:11 -0500

    check why no cache benefit from space locality

--- storage/rdma.cc
+++ storage/rdma.cc
@@ -1624,8 +1624,8 @@ ibv_qp * RDMA_Manager::create_qp(uint16_t target_node_id, bool seperated_cq, std
     qp_init_attr.recv_cq = cq1;
   qp_init_attr.cap.max_send_wr = send_outstanding_num;
   qp_init_attr.cap.max_recv_wr = recv_outstanding_num;
-  qp_init_attr.cap.max_send_sge = 5;
-  qp_init_attr.cap.max_recv_sge = 5;
+  qp_init_attr.cap.max_send_sge = 2;
+  qp_init_attr.cap.max_recv_sge = 2;
   //  qp_init_attr.cap.max_inline_data = -1;
   ibv_qp* qp = ibv_create_qp(res->pd, &qp_init_attr);
   if (!qp) {
