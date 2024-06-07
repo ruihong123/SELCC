ced75747fd6d4934963a1646ad5530f7d5f36b69
wang4996 <wang4996@purdue.edu>
2024-06-07 18:38:30 -0500

    check why no cache benefit from space locality

--- scripts/bench_micro.sh
+++ scripts/bench_micro.sh
@@ -480,15 +480,15 @@ run_node_test() {
 # node test
 echo "**************************run node test****************************"
 result_file=$bin/results/node
-node_range="1 2 4"
+node_range="1 2 4 8"
 thread_range="8 16"
 remote_range="100"
 shared_range="100"
 size_grow=0 # 0 not grow, 1 grow with node number
 read_range="0 50 95 100"
-space_range="0"
+space_range="50"
 time_range="0"
-workload_range="0 1" # 0 uniform, 1 single zipfian, n >1 multispot zipfian.
+workload_range="0" # 0 uniform, 1 single zipfian, n >1 multispot zipfian.
 zipfian_alpha_range="0.99" #make sure workload = 1 if we want to test zipfian.
 #
 op_range="1" # use 1
