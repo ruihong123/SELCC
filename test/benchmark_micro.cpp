f2eb83a8beae7892fd3d350c47104f40de4a3ead
wang4996 <wang4996@purdue.edu>
2024-06-08 00:35:30 -0500

    check why no cache benefit from space locality

--- test/benchmark_micro.cpp
+++ test/benchmark_micro.cpp
@@ -35,6 +35,8 @@
 
 //2516582ull =  48*1024*1024*1024/(2*1024)
 #define MEMSET_GRANULARITY (64*1024)
+#define EXCLUSIVE_HOTSPOT // exclusive hotspot per compute. workload == 1.
+
 uint64_t NUMOFBLOCKS = 0;
 uint64_t SYNC_KEY = 0;
 uint64_t cache_size = 0;
