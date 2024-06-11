9918c76271ddb77158676431a74ff702756196e8
wang4996 <wang4996@purdue.edu>
2024-06-11 13:31:46 -0500

    rerun the benchmark.

--- scripts/sync_all.sh
+++ scripts/sync_all.sh
@@ -114,6 +114,7 @@ function run_bench() {
   read -r -a memcached_node <<< $(head -n 1 $SRC_HOME/memcached_ip.conf)
   echo "restart memcached on ${memcached_node[0]}"
   ssh -o StrictHostKeyChecking=no ${memcached_node[0]} "sudo service memcached restart"
+#  systemctl status opensmd.service
 
 
 	}
