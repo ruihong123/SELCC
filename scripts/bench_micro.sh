04280e21e5ebd5ebfdebcdf65a3dc29c4f89bb63
wang4996 <wang4996@purdue.edu>
2024-06-12 11:03:25 -0500

    rerun the benchmark.

--- scripts/bench_micro.sh
+++ scripts/bench_micro.sh
@@ -480,7 +480,7 @@ run_node_test() {
 # node test
 echo "**************************run node test****************************"
 result_file=$bin/results/node
-node_range="1 2 4 8"
+node_range="8"
 thread_range="16"
 remote_range="100"
 shared_range="100"
@@ -488,8 +488,8 @@ size_grow=0 # 0 not grow, 1 grow with node number
 read_range="0 50 95 100"
 space_range="0"
 time_range="0"
-workload_range="1 8" # 0 uniform, 1 single zipfian, n >1 multispot zipfian.
-zipfian_theta_range="0.99" #make sure workload = 1 if we want to test zipfian.
+workload_range="1" # 0 uniform, 1 single zipfian, n >1 multispot zipfian.
+zipfian_theta_range="0.5 0.99 2" #make sure workload = 1 if we want to test zipfian.
 #
 op_range="1" # use 1
 #cache_th=0.5
