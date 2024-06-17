056385e09bf3277fcdea7df44c14789f3229770a
wang4996 <wang4996@purdue.edu>
2024-06-16 22:49:13 -0500

    return huge page

--- storage/rdma.cc
+++ storage/rdma.cc
@@ -3635,23 +3635,23 @@ int RDMA_Manager::RDMA_CAS(ibv_mr *remote_mr, ibv_mr *local_mr, uint64_t compare
         retry_cnt++;
         if (retry_cnt % INVALIDATION_INTERVAL ==  1 ) {
 //            assert(compare%2 == 0);
-            if (retry_cnt < 10) {
-//                port::AsmVolatilePause();
-                //do nothing
-            } else if (retry_cnt < 20) {
-                spin_wait_us(2);
-
-            } else if (retry_cnt < 40) {
-                spin_wait_us(8);
-            } else if (retry_cnt < 160) {
-                spin_wait_us(32);
-            } else if (retry_cnt < 200) {
-                usleep(256);
-            } else if (retry_cnt < 1000) {
-                usleep(1024);
-            } else {
-                sleep(1);
-            }
+//            if (retry_cnt < 10) {
+////                port::AsmVolatilePause();
+//                //do nothing
+//            } else if (retry_cnt < 20) {
+//                spin_wait_us(2);
+//
+//            } else if (retry_cnt < 40) {
+//                spin_wait_us(8);
+//            } else if (retry_cnt < 160) {
+//                spin_wait_us(32);
+//            } else if (retry_cnt < 200) {
+//                usleep(256);
+//            } else if (retry_cnt < 1000) {
+//                usleep(1024);
+//            } else {
+//                sleep(1);
+//            }
         }
         struct ibv_send_wr sr[2];
         struct ibv_sge sge[2];
@@ -4459,23 +4459,23 @@ int RDMA_Manager::RDMA_CAS(ibv_mr *remote_mr, ibv_mr *local_mr, uint64_t compare
 
         if (retry_cnt % INVALIDATION_INTERVAL ==  1 ) {
 //            assert(compare%2 == 0);
-            if (retry_cnt < 10) {
-//                port::AsmVolatilePause();
-                //do nothing
-            } else if (retry_cnt < 20) {
-                spin_wait_us(2);
-
-            } else if (retry_cnt < 40) {
-                spin_wait_us(8);
-            } else if (retry_cnt < 160) {
-                spin_wait_us(32);
-            } else if (retry_cnt < 200) {
-                usleep(256);
-            } else if (retry_cnt < 1000) {
-                usleep(1024);
-            } else {
-                sleep(1);
-            }
+//            if (retry_cnt < 10) {
+////                port::AsmVolatilePause();
+//                //do nothing
+//            } else if (retry_cnt < 20) {
+//                spin_wait_us(2);
+//
+//            } else if (retry_cnt < 40) {
+//                spin_wait_us(8);
+//            } else if (retry_cnt < 160) {
+//                spin_wait_us(32);
+//            } else if (retry_cnt < 200) {
+//                usleep(256);
+//            } else if (retry_cnt < 1000) {
+//                usleep(1024);
+//            } else {
+//                sleep(1);
+//            }
         }
 //        if (retry_cnt > 180000) {
 //            std::cout << "Deadlock for write lock " << lock_addr << std::endl;
