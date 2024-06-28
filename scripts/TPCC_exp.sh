7e19d010427b204e575d47e6d02956aae3c2c142
wang4996 <wang4996@purdue.edu>
2024-06-28 13:41:20 -0500

    finish the implementation for Timestamp based concurrency control.

--- scripts/TPCC_exp.sh
+++ scripts/TPCC_exp.sh
@@ -128,7 +128,7 @@ vary_query_ratio () {
   FREQUENCY_NEW_ORDER=(0 0 100 0 0)
   FREQUENCY_ORDER_STATUS=(0 0 0 100 0)
   FREQUENCY_STOCK_LEVEL=(0 0 0 0 100)
-  for qr_index in 0 1 2 3 4; do
+  for qr_index in 1 0 2 3 4; do
     for ware_num in ${WarehouseNum[@]}; do
       for thread_n in ${thread_number[@]}; do
         compute_ARGS="-p$port -sf$ware_num -sf1 -c$thread_n -rde${FREQUENCY_DELIVERY[$qr_index]} -rpa${FREQUENCY_PAYMENT[$qr_index]} -rne${FREQUENCY_NEW_ORDER[$qr_index]} -ror${FREQUENCY_ORDER_STATUS[$qr_index]} -rst${FREQUENCY_STOCK_LEVEL[$qr_index]} -t1000000 -f../connection.conf"
@@ -137,24 +137,24 @@ vary_query_ratio () {
     done
   done
 }
-vary_query_ratio2 () {
-  #read_ratios=(0 30 50 70 90 100)
-  thread_number=(8)
-  WarehouseNum=(64 256)
-  FREQUENCY_DELIVERY=(100 0 0 0 0)
-  FREQUENCY_PAYMENT=(0 100 0 0 0)
-  FREQUENCY_NEW_ORDER=(0 0 100 0 0)
-  FREQUENCY_ORDER_STATUS=(0 0 0 100 0)
-  FREQUENCY_STOCK_LEVEL=(0 0 0 0 100)
-  for qr_index in 0 1 2 3 4; do
-    for ware_num in ${WarehouseNum[@]}; do
-      for thread_n in ${thread_number[@]}; do
-        compute_ARGS="-p$port -sf$ware_num -sf1 -c$thread_n -rde${FREQUENCY_DELIVERY[$qr_index]} -rpa${FREQUENCY_PAYMENT[$qr_index]} -rne${FREQUENCY_NEW_ORDER[$qr_index]} -ror${FREQUENCY_ORDER_STATUS[$qr_index]} -rst${FREQUENCY_STOCK_LEVEL[$qr_index]} -t1000000 -f../connection.conf"
-        run_tpcc
-      done
-    done
-  done
-}
+#vary_query_ratio2 () {
+#  #read_ratios=(0 30 50 70 90 100)
+#  thread_number=(8)
+#  WarehouseNum=(64 256)
+#  FREQUENCY_DELIVERY=(100 0 0 0 0)
+#  FREQUENCY_PAYMENT=(0 100 0 0 0)
+#  FREQUENCY_NEW_ORDER=(0 0 100 0 0)
+#  FREQUENCY_ORDER_STATUS=(0 0 0 100 0)
+#  FREQUENCY_STOCK_LEVEL=(0 0 0 0 100)
+#  for qr_index in 0 1 2 3 4; do
+#    for ware_num in ${WarehouseNum[@]}; do
+#      for thread_n in ${thread_number[@]}; do
+#        compute_ARGS="-p$port -sf$ware_num -sf1 -c$thread_n -rde${FREQUENCY_DELIVERY[$qr_index]} -rpa${FREQUENCY_PAYMENT[$qr_index]} -rne${FREQUENCY_NEW_ORDER[$qr_index]} -ror${FREQUENCY_ORDER_STATUS[$qr_index]} -rst${FREQUENCY_STOCK_LEVEL[$qr_index]} -t1000000 -f../connection.conf"
+#        run_tpcc
+#      done
+#    done
+#  done
+#}
 
 vary_temp_locality () {
   #localities=(0 30 50 70 90 100)
