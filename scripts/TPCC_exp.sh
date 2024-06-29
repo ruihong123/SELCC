71ba4243fc8d0c51bf47c474f933dc646c82bf18
wang4996 <wang4996@purdue.edu>
2024-06-28 22:43:51 -0500

    finish the implementation for Timestamp based concurrency control.

--- scripts/TPCC_exp.sh
+++ scripts/TPCC_exp.sh
@@ -128,8 +128,8 @@ vary_query_ratio () {
   FREQUENCY_NEW_ORDER=(0 0 100 0 0 20)
   FREQUENCY_ORDER_STATUS=(0 0 0 100 0 20)
   FREQUENCY_STOCK_LEVEL=(0 0 0 0 100 20)
-  for qr_index in 0 1 2 3 4 5; do
-    for ware_num in ${WarehouseNum[@]}; do
+  for ware_num in ${WarehouseNum[@]}; do
+    for qr_index in 0 1 2 3 4 5; do
       for thread_n in ${thread_number[@]}; do
         compute_ARGS="-p$port -sf$ware_num -sf1 -c$thread_n -rde${FREQUENCY_DELIVERY[$qr_index]} -rpa${FREQUENCY_PAYMENT[$qr_index]} -rne${FREQUENCY_NEW_ORDER[$qr_index]} -ror${FREQUENCY_ORDER_STATUS[$qr_index]} -rst${FREQUENCY_STOCK_LEVEL[$qr_index]} -t1000000 -f../connection.conf"
         run_tpcc
