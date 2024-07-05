e8489e8c88f556b2d2326f2806d84d3cab9a9a84
wang4996 <wang4996@purdue.edu>
2024-07-05 10:29:01 -0500

    finish the implementation for Timestamp based concurrency control.

--- scripts/TPCC_exp.sh
+++ scripts/TPCC_exp.sh
@@ -29,7 +29,7 @@ output_dir="/users/Ruihong/MemoryEngine/scripts/data"
 core_dump_dir="/mnt/core_dump"
 # working environment
 proj_dir="/users/Ruihong/MemoryEngine"
-bin_dir="${proj_dir}/release"
+bin_dir="${proj_dir}/debug"
 script_dir="${proj_dir}/database/scripts"
 ssh_opts="-o StrictHostKeyChecking=no"
 
@@ -121,7 +121,7 @@ vary_read_ratios () {
 
 vary_query_ratio () {
   #read_ratios=(0 30 50 70 90 100)
-  thread_number=(8)
+  thread_number=(1)
   WarehouseNum=(256)
   FREQUENCY_DELIVERY=(100 0 0 0 0 20 33 0)
   FREQUENCY_PAYMENT=(0 100 0 0 0 20 33 0)
@@ -129,7 +129,7 @@ vary_query_ratio () {
   FREQUENCY_ORDER_STATUS=(0 0 0 100 0 20 0 50)
   FREQUENCY_STOCK_LEVEL=(0 0 0 0 100 20 0 50)
   for ware_num in ${WarehouseNum[@]}; do
-    for qr_index in 0 1 2 3 4 5 6 7; do
+    for qr_index in 2; do
       for thread_n in ${thread_number[@]}; do
         compute_ARGS="-p$port -sf$ware_num -sf1 -c$thread_n -rde${FREQUENCY_DELIVERY[$qr_index]} -rpa${FREQUENCY_PAYMENT[$qr_index]} -rne${FREQUENCY_NEW_ORDER[$qr_index]} -ror${FREQUENCY_ORDER_STATUS[$qr_index]} -rst${FREQUENCY_STOCK_LEVEL[$qr_index]} -t100000 -f../connection.conf"
         run_tpcc
