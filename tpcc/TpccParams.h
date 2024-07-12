52140e0ea72aa6718a17ad01a4281ce167012367
wang4996 <wang4996@purdue.edu>
2024-07-11 23:49:38 -0500

    finish the implementation for Timestamp based concurrency control.

--- tpcc/TpccParams.h
+++ tpcc/TpccParams.h
@@ -9,9 +9,9 @@
 #include <cassert>
 
 #define LOGGING true
-#define PARTITIONED false
-//#define WORKLOAD_PATTERN SourceType::PARTITION_SOURCE
-#define WORKLOAD_PATTERN SourceType::RANDOM_SOURCE
+#define PARTITIONED true
+#define WORKLOAD_PATTERN SourceType::PARTITION_SOURCE
+//#define WORKLOAD_PATTERN SourceType::RANDOM_SOURCE
 namespace DSMEngine {
 namespace TpccBenchmark {
 struct TpccScaleParams {
