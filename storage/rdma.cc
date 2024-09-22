ce9ab2618347d3a74661b380c2dd09d892a2b2ae
ruihong123 <wang4996@purdue.edu>
2024-09-22 14:52:10 -0400

    invalidation message buffer reshape.

--- storage/rdma.cc
+++ storage/rdma.cc
@@ -4935,6 +4935,7 @@ int RDMA_Manager::RDMA_CAS(ibv_mr *remote_mr, ibv_mr *local_mr, uint64_t compare
 #ifndef NDEBUG
             if(((*(uint64_t*) local_CAS_mr->addr) >> 56) != (add >> 56)){
                 printf("RDMA write handover move too fast, resulting in spurious latch word mismatch\n");
+                fflush(stdout);
                 usleep(40);
                 //RDMA read the latch word again and see if it is the same as the compare value.
                 RDMA_Read(remote_lock_addr, local_CAS_mr, 8, IBV_SEND_SIGNALED,1, Regular_Page);
