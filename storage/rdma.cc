83601dae679a7df3805cfbf3524203dbe1510782
ruihong123 <wang4996@purdue.edu>
2024-09-23 00:40:05 -0400

    invalidation message buffer reshape.

--- storage/rdma.cc
+++ storage/rdma.cc
@@ -5254,7 +5254,7 @@ int RDMA_Manager::RDMA_CAS(ibv_mr *remote_mr, ibv_mr *local_mr, uint64_t compare
                 usleep(40);
                 //RDMA read the latch word again and see if it is the same as the compare value.
                 RDMA_Read(remote_lock_addr, local_CAS_mr, 8, IBV_SEND_SIGNALED,1, Regular_Page);
-                assert((*(uint64_t*)local_CAS_mr->addr & inv_sender_shared) != 0);
+//                assert((*(uint64_t*)local_CAS_mr->addr & inv_sender_shared) != 0);
                 assert((*(uint64_t*)local_CAS_mr->addr & this_node_shared) != 0);
                 assert(((*(uint64_t*)local_CAS_mr->addr) >> 56) == 0);
 //                assert(((*(uint64_t*) local_CAS_mr->addr) >> 56) == add >> 56);
