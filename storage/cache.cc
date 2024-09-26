90be84100481f63e0ea9cd4f4a59adc3c2e37472
ruihong123 <wang4996@purdue.edu>
2024-09-25 23:27:01 -0400

    invalidation message buffer reshape.

--- storage/cache.cc
+++ storage/cache.cc
@@ -1710,7 +1710,7 @@ LocalBuffer::LocalBuffer(const CacheConfig &cache_config) {
 //                rdma_mg->global_write_page_and_WHandover(mr, page_addr, page_size, buffer_inv_message.next_holder_id.load(), lock_addr,
 //                                                         false, nullptr);
                 rdma_mg->global_WHandover(mr, page_addr, page_size, buffer_inv_message.next_holder_id.load(), lock_addr,
-                                                         false, nullptr);
+                                                         true, nullptr);
                 // The sequence of this two RDMA message could be problematic, because we do not know,
                 // whether the global latch release will arrive sooner than the page forward. if not the next cache holder
                 // can find the old cach copy holder still there when releasing the latch.
