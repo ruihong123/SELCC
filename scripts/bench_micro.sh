e605ca7aee9cea95beb0ab47772c4181bb74efdc
ruihong123 <wang4996@purdue.edu>
2024-10-01 14:02:51 -0400

    Enable dirty only flush back.

--- scripts/bench_micro.sh
+++ scripts/bench_micro.sh
@@ -2,7 +2,7 @@
 bin=`dirname "$0"`
 bin=`cd "$bin"; pwd`
 SRC_HOME=$bin/..
-BIN_HOME=$bin/../release
+BIN_HOME=$bin/../debug
 conf_file_all=$bin/../connection_cloudlab.conf
 conf_file=$bin/../connection.conf
 # alpha = 1/(1-theta)
@@ -482,11 +482,11 @@ run_node_test() {
 echo "**************************run node test****************************"
 result_file=$bin/results/node
 node_range="8"
-thread_range="1 2 4"
+thread_range="8"
 remote_range="100"
 shared_range="100"
 size_grow=0 # 0 not grow, 1 grow with node number
-read_range="100"
+read_range="0 50 95 100"
 space_range="0"
 time_range="0"
 workload_range="0" # 0 uniform, 1 single zipfian, n >1 multispot zipfian.
