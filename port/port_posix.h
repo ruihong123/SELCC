81fd3efdf431d8661d63f874f88f42de31a7ddee
ruihong123 <wang4996@purdue.edu>
2024-10-03 23:10:34 -0400

    Enable dirty only flush back.

--- port/port_posix.h
+++ port/port_posix.h
@@ -124,7 +124,7 @@ extern bool Show_Me_The_Print;
 #define PARALLEL_INVALIDATION
 #define PAGE_FREE_LIST
 #define FREELIST_RATIO 0.01
-#define FREELIST_THREAD_NUM 1
+#define FREELIST_THREAD_NUM 2
 #define DIRTY_ONLY_FLUSH
 #define ASYNC_PLAN 1
 //#define OPS_TIMER
