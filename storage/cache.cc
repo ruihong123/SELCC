a46e0bb7e8cc0aed058c21bdf464342be791b41f
wang4996 <wang4996@purdue.edu>
2024-10-14 23:36:12 -0500

    Rename SELCC APIs

--- storage/cache.cc
+++ storage/cache.cc
@@ -1783,12 +1783,12 @@ LocalBuffer::LocalBuffer(const CacheConfig &cache_config) {
                 // TODO: need to use a local buffer to support the asynchronous RDMA page forward.
                 rdma_mg->RDMA_Write_xcompute(local_mr, buffer_inv_message.next_receive_page_buf,
                                              buffer_inv_message.next_receive_rkey, kLeafPageSize,
-                                             buffer_inv_message.next_holder_id, qp_id, false, false);
+                                             buffer_inv_message.next_holder_id, qp_id, false, true);
                 //TODO: The dirty page flush back here is not necessary.
 //                rdma_mg->global_write_page_and_WHandover(mr, page_addr, page_size, buffer_inv_message.next_holder_id.load(), lock_addr,
 //                                                         false, nullptr);
                 rdma_mg->global_WHandover(mr, page_addr, page_size, buffer_inv_message.next_holder_id.load(), lock_addr,
-                                                         false, nullptr);
+                                                         true, nullptr);
                 // The sequence of this two RDMA message could be problematic, because we do not know,
                 // whether the global latch release will arrive sooner than the page forward. if not the next cache holder
                 // can find the old cach copy holder still there when releasing the latch.
