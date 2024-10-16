f5c28113d41f49b15668a1e3980c0e817c557387
ruihong123 <wang4996@purdue.edu>
2024-10-15 23:37:42 -0400

    Enable dirty only flush back.

--- storage/rdma.cc
+++ storage/rdma.cc
@@ -5126,7 +5126,7 @@ int RDMA_Manager::RDMA_CAS(ibv_mr *remote_mr, ibv_mr *local_mr, uint64_t compare
 #ifndef NDEBUG
 
 
-
+                uint64_t initial_old_cas = (*(uint64_t*) local_CAS_mr->addr);
                 if(((*(uint64_t*) local_CAS_mr->addr) >> 56) != (compare >> 56)){
                     size_t count = 0;
 
