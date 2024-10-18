f2c2d756a452b002c2741902c9bd28b5f0566534
ruihong123 <wang4996@purdue.edu>
2024-10-17 20:30:26 -0400

    Enable dirty only flush back.

--- scripts/btree_exp.sh
+++ scripts/btree_exp.sh
@@ -112,7 +112,7 @@ run_tpcc () {
 vary_thread_number () {
   #read_ratios=(0 30 50 70 90 100)
   thread_number=(8)
-  read_ratio=(50)
+  read_ratio=(0)
   # shellcheck disable=SC2068
   for thread_n in ${thread_number[@]}; do
     for read_r in ${read_ratio[@]}; do
