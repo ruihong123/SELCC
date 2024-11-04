911c129317e8309c350265a1517d1989b6234123
ruihong123 <wang4996@purdue.edu>
2024-11-04 14:54:11 -0500

    Clean up the warning

--- scripts/bench_micro.sh
+++ scripts/bench_micro.sh
@@ -9,7 +9,7 @@ conf_file=$bin/../connection.conf
 #compute_nodes=$bin/compute_nodes
 #memory_nodes=$bin/memory_nodes
 log_file=$bin/log
-cache_mem_size=8 # 8 gb Local memory size
+cache_mem_size=16 # 8 gb Local memory size
 remote_mem_size_base=48 # 48 gb Remote memory size
 #master_ip=db3.cs.purdue.edu # make sure this is in accordance with the server whose is_master=1
 master_port=12311
