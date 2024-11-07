5f30212bbf837cbf0be073fc85251bec3d63c3b3
ruihong123 <wang4996@purdue.edu>
2024-11-07 13:43:11 -0500

    Clean up the warning

--- scripts/db_TPCC_exp.sh
+++ scripts/db_TPCC_exp.sh
@@ -104,6 +104,7 @@ run_tpcc () {
 run_tpcc_dist () {
 #  dist_ratios=(0 10 20 30 40 50 60 70 80 90 100)
   dist_ratios=(0 30 60 100)
+  dist_ratios=(0)
 #  dist_ratios=(100)
 
   for dist_ratio in ${dist_ratios[@]}; do
@@ -154,14 +155,14 @@ vary_query_ratio () {
 vary_distro_ratio () {
   #read_ratios=(0 30 50 70 90 100)
   thread_number=(8)
-  WarehouseNum=(512)
+  WarehouseNum=(256)
   FREQUENCY_DELIVERY=(100 0 0 0 0 1)
   FREQUENCY_PAYMENT=(0 100 0 0 0 10)
   FREQUENCY_NEW_ORDER=(0 0 100 0 0 10)
   FREQUENCY_ORDER_STATUS=(0 0 0 100 0 1)
   FREQUENCY_STOCK_LEVEL=(0 0 0 0 100 1)
   for ware_num in ${WarehouseNum[@]}; do
-    for qr_index in 1 2; do
+    for qr_index in 1; do
       for thread_n in ${thread_number[@]}; do
         compute_ARGS="-p$port -sf$ware_num -sf1 -c$thread_n -rde${FREQUENCY_DELIVERY[$qr_index]} -rpa${FREQUENCY_PAYMENT[$qr_index]} -rne${FREQUENCY_NEW_ORDER[$qr_index]} -ror${FREQUENCY_ORDER_STATUS[$qr_index]} -rst${FREQUENCY_STOCK_LEVEL[$qr_index]} -t8000000 -f../connection.conf"
         run_tpcc_dist
