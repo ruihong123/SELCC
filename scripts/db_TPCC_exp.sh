a0b7b2b6ec25132cb4eabda799f409fd5429edca
ruihong123 <wang4996@purdue.edu>
2024-11-23 23:40:03 -0500

    Clean up the warning

--- scripts/db_TPCC_exp.sh
+++ scripts/db_TPCC_exp.sh
@@ -70,7 +70,7 @@ launch () {
         script_memory="cd ${bin_dir} && ./memory_server_tpcc $port $(($remote_mem_size)) $((2*$i +1)) > ${output_file} 2>&1"
         echo "start worker: ssh ${ssh_opts} ${memory} '$script_memory' &"
 #        ssh ${ssh_opts} ${memory} "echo '$core_dump_dir/core$memory' | sudo tee /proc/sys/kernel/core_pattern"
-        ssh ${ssh_opts} ${memory} " $script_memory" &
+        ssh ${ssh_opts} ${memory} " ulimit -S -c 10 &&  $script_memory" &
         sleep 1
   done
   script_compute="cd ${bin_dir} && ./tpcc ${compute_ARGS} -d${dist_ratio}"
