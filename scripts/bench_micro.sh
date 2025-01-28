bf0dd40fe46ecc28ef87c43288b0c624f89750ef
ruihong123 <wang4996@purdue.edu>
2025-01-28 11:39:15 -0500

    do scalability test

--- scripts/bench_micro.sh
+++ scripts/bench_micro.sh
@@ -170,10 +170,10 @@ run_node_test() {
 # node test
 echo "**************************run node test****************************"
 result_file=$bin/results/node
-node_range="16"
+node_range="1 2 4 8"
 thread_range="8"
 remote_range="100"
-shared_range="0 30 60 100"
+shared_range="30 60 100"
 size_grow=0 # 0 not grow, 1 grow with node number
 read_range="0 50 95"
 space_range="0"
