c65dbca1a3694f31da72e3caa4dc57c665310dc9
ruihong123 <wang4996@purdue.edu>
2025-01-30 13:55:38 -0500

    do scalability test

--- scripts/bench_micro.sh
+++ scripts/bench_micro.sh
@@ -142,6 +142,14 @@ run() {
         else
           is_master=0
         fi
+
+        if [ $single_writer = 1  ]; then
+            if [ $i = 0 ]; then
+              $read_ratio="0"
+            else
+              $read_ratio="100"
+            fi
+        fi
 #        if [ $port == $ip ]; then
 #          port=12345
 #        fi
@@ -231,15 +239,16 @@ run_skewed_test() {
 echo "**************************run node test****************************"
 result_file=$bin/results/node
 node_range="8"
-thread_range="1 2 4 8"
+thread_range="16"
 remote_range="100"
 shared_range="100"
 size_grow=0 # 0 not grow, 1 grow with node number
+single_writer=0
 read_range="50"
 space_range="0"
 time_range="0"
 workload_range="1" # 0 uniform, 1 single zipfian, n >1 multispot zipfian.
-zipfian_alpha_range="0.1 0.99 10" #make sure workload = 1 if we want to test zipfian.
+zipfian_alpha_range="10" #make sure workload = 1 if we want to test zipfian.
 #
 op_range="1" # use 1
 #cache_th=0.5
