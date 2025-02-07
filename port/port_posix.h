c191de870ee357c18f7c64bbcba3b0c2198d152c
ruihong123 <wang4996@purdue.edu>
2025-02-07 14:24:27 -0500

    try to trigger starvation.

--- port/port_posix.h
+++ port/port_posix.h
@@ -130,7 +130,7 @@ extern bool Show_Me_The_Print;
 
 #define PARALLEL_DEGREE 8
 #define STARVATION_THRESHOLD 256 // todo: we can try 1, 8 64 and UINT64_MAX
-#define WRITER_STARV_SPIN_BASE 8
+//#define WRITER_STARV_SPIN_BASE 8
 //#define STARV_REVENGE
 #define STARV_BACKOFF
 
