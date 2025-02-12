3f1c3f6631994a09d4a72d1fb22f23e1cf6a5672
ruihong123 <wang4996@purdue.edu>
2025-02-11 22:29:31 -0500

    try to trigger starvation.

--- include/Common.h
+++ include/Common.h
@@ -53,9 +53,9 @@
 
 #define DIR_MESSAGE_NR 128
 
-#define kInternalPageSize (2048 + 8)
+#define kInternalPageSize (4096 + 8)
 
-#define kLeafPageSize (2048 + 8)
+#define kLeafPageSize (4096 + 8)
 #define kDataPageSize kLeafPageSize
 
 #define KEY_PADDING 12
