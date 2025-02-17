18323fc5da464cadecf924aaa43e0dbec1c018fb
ruihong123 <wang4996@purdue.edu>
2025-02-17 16:47:25 -0500

    redo the scalaibility test with large number of compute nodes.

--- scripts/bench_micro.sh
+++ scripts/bench_micro.sh
@@ -237,12 +237,12 @@ run_node_test() {
 # node test
 echo "**************************run node test****************************"
 result_file=$bin/results/node
-node_range="16"
+node_range="1 2 4 8"
 thread_range="8"
 remote_range="100"
 shared_range="0 30 60 100"
 size_grow=0 # 0 not grow, 1 grow with node number
-read_range="0 50 95"
+read_range="0 50 95 100"
 space_range="0"
 time_range="0"
 workload_range="0" # 0 uniform, 1 single zipfian, n >1 multispot zipfian.
@@ -364,6 +364,6 @@ done
 #run_remote_test
 #run_space_test
 #run_shared_test_noeviction
-#run_node_test
-run_test
+run_node_test
+#run_test
 #run_skewed_test
