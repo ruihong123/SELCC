1df605646772c8855bb8e3f64f8c98accdf244c0
ruihong123 <wang4996@purdue.edu>
2025-02-18 17:08:06 -0500

    redo the scalaibility test with large number of compute nodes.

--- scripts/bench_micro.sh
+++ scripts/bench_micro.sh
@@ -237,7 +237,7 @@ run_node_test() {
 # node test
 echo "**************************run node test****************************"
 result_file=$bin/results/node
-node_range="8"
+node_range="16"
 thread_range="8"
 remote_range="100"
 shared_range="0 30 60 100"
@@ -309,7 +309,7 @@ time_range="0"
 workload_range="1" # 0 uniform, 1 single zipfian, n >1 multispot zipfian. -1 accesss only single cache line
 zipfian_alpha_range="10" #make sure workload = 1 if we want to test zipfian.
 #
-op_range="1" # use 1, 2 for operation with spinning
+op_range="1" # use 1, 2 is for operation with spinning
 #cache_th=0.5
 for workload in $workload_range
 do
